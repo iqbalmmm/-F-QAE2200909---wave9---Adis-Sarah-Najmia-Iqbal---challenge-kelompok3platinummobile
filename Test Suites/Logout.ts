f5f6db4699b0425a0ecb75ad4ed8c4d9abaa575a
iqbalmmm <iqbal.itang@gmail.com>
2023-07-24 21:51:59 +0700

    add features edit item notification logout

---
+++ Test Suites/Logout.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Logout</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>b3ce5521-fbea-497a-9c77-ec98009e60e8</testSuiteGuid>
+   <testCaseLink>
+      <guid>2480bde3-523b-4045-b906-a765debb2d63</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Pages/Logout/Click Logout</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
