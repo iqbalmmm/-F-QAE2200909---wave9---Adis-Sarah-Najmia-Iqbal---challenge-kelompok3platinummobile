1b3f185fd63e8bf6c186601b6c2a4899354814c3
sarahsiahaan99 <sarahsiahaan7@gmail.com>
2023-07-24 22:32:47 +0700

    Push sarah

---
+++ Test Suites/Sell List.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Sell List</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>473ed8ea-8823-426a-b98b-385246b3357b</testSuiteGuid>
+   <testCaseLink>
+      <guid>aed8cf68-9e7a-46c4-9352-a9a7648e5ebc</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Step Definition/Feature Sell List/User (Seller) want to see list of the items that the User (Seller) is selling</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
